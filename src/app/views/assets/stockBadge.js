1a64381a83a05525f7730284fdc30abb9cb66506
destro <morrisdestro@gmail.com>
2024-03-14 20:39:31 +0300

    minor fix

--- src/app/views/assets/stockBadge.js
+++ src/app/views/assets/stockBadge.js
@@ -4,16 +4,16 @@ import Chip from '@mui/material/Chip';
 const StockBadge = ({ value }) => {
   let color = 'error';
   let stock_message = '';
-  if (value > 4 && value < 10) {
-    color = 'warning';
-    stock_message = value + ' available';
-  } else if (value <= 4) {
+  if (value <= 3) {
+    color = 'error';
+    stock_message = 'out of stock';
+  } else if (value >= 4 && value < 10) {
     color = 'warning';
     stock_message = value + ' available';
   } else if (value >= 10 && value <= 20) {
     color = 'primary';
     stock_message = value + ' available';
-  } else {
+  } else if (value > 20) {
     color = 'success';
     stock_message = 'in stock';
   }
